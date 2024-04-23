22fd49fa7c99973d8238afe4681f479120e8ac52
andreannov <160991378+andreannov@users.noreply.github.com>
2024-04-23 12:06:47 +0700

    Add files via upload

---
+++ kelasPengganti/cobaMyData.java
@@ -0,0 +1,8 @@
+package kelasPengganti;
+
+public class cobaMyData {
+    public static void main(String[] args) {
+        myData<String> data = new myData<>("ian");
+        System.out.println(data.getData());
+    }
+}
