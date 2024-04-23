b9bff044ee9676b421ff48811b1c14384637af6f
andreannov <160991378+andreannov@users.noreply.github.com>
2024-04-23 12:12:23 +0700

    Update cobaGeneric.java

--- kelasPengganti/cobaGeneric.java
+++ kelasPengganti/cobaGeneric.java
@@ -2,29 +2,30 @@ package kelasPengganti;
 
 public class cobaGeneric {
     public static void main(String[] args) {
+            public static void main(String[] args) {
         //Membuat Instance dan Mendefinisikan Tipe Data String
-            generic<String> nama = new generic()();
-            nama.setOrang("Wildan");
+            generic<String> nama = new generic<>();
+            nama.setOrang("Ian");
             System.out.println("Nama Saya: "+nama.getOrang());
 
         //Membuat Instance dan Mendefinisikan Tipe Data Integer
-            generic<Integer> umur = new generic()();
-            umur.setOrang(19);
+            generic<Integer> umur = new generic<>();
+            umur.setOrang(23);
             System.out.println("Usia Saya: "+umur.getOrang()+" Tahun");
 
         //Membuat Instance dan Mendefinisikan Tipe Data Boolean
-            generic<Boolean> kondisi = new generic()();
+            generic<Boolean> kondisi = new generic<>();
             kondisi.setOrang(true);
             System.out.println("Saya Sedang Belajar Java: "+kondisi.getOrang());
 
         //Membuat Instance dan Mendefinisikan Tipe Data Float
-            generic<Float> IPK = new generic();
+            generic<Float> IPK = new generic<>();
             IPK.setOrang(3.5f);
             System.out.println("Nilai IPK Saya: "+IPK.getOrang());
             
         //Membuat Instance dan Mendefinisikan Tipe Data Character
-            generic<Character> Karakter = new generic()();
-            Karakter.setOrang('W');
+            generic<Character> Karakter = new generic<>();
+            Karakter.setOrang('I');
             System.out.println("Huruf Awalan Nama Saya Adalah: "+Karakter.getOrang());
     }
 }
